1886046d63d10a5ab5bad55d443536801ac52f3f
wailHAMLAOUI <w_hamlaoui@estin.dz>
2023-04-04 16:30:49 +0100

    update

--- src/scenes/bookSearch/BookSearch.jsx
+++ src/scenes/bookSearch/BookSearch.jsx
@@ -1,19 +1,36 @@
-import React  from 'react'
-// import Navbar1 from '../navbar/Navbar1'
+import React,{ useState }   from 'react'
+import searchIcon from '../images/icons8-search-100 2.png';
+import "./BookSearch.css"
 
 export default function BookSearch() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
 
   return (
-    <div>
-        {/* <div>
-            <Navbar1/>
-        </div> */}
-        <div>
+    <div className='Search'>
+        <div className='bookSearchLeft'>
+
+          <div className="search-bar">
+            <img src={searchIcon} alt="Search Icon" />
+            <input
+                type="text"
+                placeholder="Search Book Summary ...."
+                value={searchText}
+                onChange={handleSearch}
+              />
+          </div>
+
+          
 
         </div>
-        <div>
+
+        <div className='bookSearchRight'>
             
         </div>
+
     </div>
   )
 }
